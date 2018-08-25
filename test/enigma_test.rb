1e52bbb3cbcf3a6f75b439aa1e69e39509d112fb
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-08-25 16:43:09 -0600

    create message string test and refactor encryp method

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -8,15 +8,16 @@ require 'pry'
 class EnigmaTest < Minitest::Test
   def test_it_exists
     e = Enigma.new
-
     assert_instance_of Enigma, e
   end
 
   def test_the_message_string
     e = Enigma.new
     my_message = "this is so secret ..end.."
-
-    assert_equal "this is so secret ..end..", e.encrypt(my_message, "12345", Date.today)
-
+    expected = "this is so secret ..end.."
+    assert_equal expected, e.encrypt(my_message, "12345", Date.today)
   end
+
+
+  
 end
