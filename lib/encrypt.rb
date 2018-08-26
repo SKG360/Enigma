82d81e55f65d898d81e31e1b346fe9cdc60746f5
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-08-25 20:54:20 -0600

    create check local directory method and test

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -15,6 +15,10 @@ class Encrypt
     input_filename = @input_filename
     output_filename = @output_filename
   end
+
+  def check_local_files
+    @directory_files = Dir.glob("*.txt")
+  end
   #input_filename => ARGV[0] => message.txt
   #output_filename => ARGV[1] => encrypted.txt
 end
--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -5,6 +5,14 @@ require './lib/encrypt'
 
 class EncryptTest < Minitest::Test
 
+
+    def test_it_can_read_local_directory
+        e = Encrypt.new("Hello World", "message.txt")
+        assert_equal ["message.txt"], e.check_local_files
+    end
+
+    
+
     def test_recieves_first_arg
         #message to encrypt
     end
