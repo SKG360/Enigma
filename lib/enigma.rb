b437dfd888f09d14b727fbb2dff0f80c85b052c9
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-08-27 15:15:02 -0600

    update

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -9,8 +9,6 @@ class Enigma
     @todays_date  = Date.today.strftime("%d%m%y")
   end
 
-<<<<<<< HEAD
-=======
   def encrypt(my_message, key = random_key, date = @todays_date)
     @date         = date
     @key          = key
@@ -18,30 +16,15 @@ class Enigma
 
     # return encrypted_text(my_message, key, @date - @todays_date).join
   end
->>>>>>> af1180a301cbcdf3313c2f479b056d8e9b68b2ce
 
   def character_map
     letters_array = [*'a'..'z']
     numbers_array = [*'0'..'9']
     other = [" ", ".", ","]
-<<<<<<< HEAD
-    @characters = letters_array + numbers_array + other
-=======
     return letters_array + numbers_array + other
->>>>>>> af1180a301cbcdf3313c2f479b056d8e9b68b2ce
   end # => full character array [a...,].count => 39
 
-  def locate_self_in_array(unit)
-    @index_location = 0
-    while character_map[@index_location] != unit
-      if character_map[@index_location] == unit
-        return @index_location
-      elsif @index_location += 1
-      end
-    end
-    return @index_location
-  end
-  # default value 
+   
   def random_key 
     key_string = ""
     5.times do
@@ -50,40 +33,6 @@ class Enigma
     return key_string
   end # => five digit string
 
-<<<<<<< HEAD
-  # rename
-  def encrypted_char(part)
-      # date  =>  "240818"
-      
-      char_array    = character_map
-      # char_array.count = 39
-
-      date_square   = (@todays_date.to_i ** 2).to_s 
-                                     # => "62909669124"
-                                            # => "9124"
-      offset_A      = date_square[-4].to_i  # => "9"
-      offset_B      = date_square[-3].to_i  # =>  "1"
-      offset_C      = date_square[-2].to_i  # =>   "2"
-      offset_D      = date_square[-1].to_i  # =>    "4"
-
-      @key = "82648"
-      key_pair_A    = @key[0..1].to_i       # => "82"
-      key_pair_B    = @key[1..2].to_i       # =>  "26"
-      key_pair_C    = @key[2..3].to_i       # =>   "64"
-      key_pair_D    = @key[3..4].to_i       # =>    "48"
-
-      rotation_A    = offset_A + key_pair_A # =>  9+82 = 91
-      rotation_B    = offset_B + key_pair_B # =>   1+26 = 27 
-      rotation_C    = offset_C + key_pair_C # =>    2+64 = 66
-      rotation_D    = offset_D + key_pair_D # =>     4+48 = 52
-
-  end
-
-
-  def encrypt_four(four_character_string_array)
-    four_character_string_array.map do |character|
-      locate_self_in_array(character.downcase)
-=======
   # date.strftime("%d%m%y")
   def encrypted_piece(part)
     # date  =>  "240818"
@@ -106,23 +55,9 @@ class Enigma
       sum = char_array.index(character) + rotation + offset
           # => "110"
       encrypted_char = char_array[sum % char_array.length]
->>>>>>> af1180a301cbcdf3313c2f479b056d8e9b68b2ce
     end
   end
 
-<<<<<<< HEAD
-  def message_array(message)
-    message_slices = message.chars.each_slice(slice_length).to_a
-    # => [["H", "i", " ", "t"], ["h", "e", "r", "e"], ["!"]]
-
-  end
-
-  def encryption_array(slice_length)
-
-    encrypted_message_slices = []
-    message_slices.map do |four_digit_slice|
-      encrypt_four(four_digit_slice)
-=======
     def encrypted_parts
       # turn message into parts
       message_parts = @my_message.chars.each_slice(4).to_a
@@ -139,7 +74,7 @@ class Enigma
           decrypted_section(slice)
       end
       decrypted_message_slices
-require "pry"; binding.pry
+      require "pry"; binding.pry
     end
 
     def decrypted_section(slice)
@@ -162,7 +97,6 @@ require "pry"; binding.pry
 
       encrypted_char = char_array[sum % char_array.length]
 
->>>>>>> af1180a301cbcdf3313c2f479b056d8e9b68b2ce
     end
     encrypted_message_slices.join
   end
@@ -176,16 +110,9 @@ require "pry"; binding.pry
   end
 end
 
-<<<<<<< HEAD
-e = Enigma.new
-# p e.encrypt("Hi there!")
-
-
-=======
 
   # def encrypted_text(message)
   #   message_array(message).map do |four_digit_slice|
   #     encrypt_four(four_digit_slice)
   #   end.join
   # end
->>>>>>> af1180a301cbcdf3313c2f479b056d8e9b68b2ce
