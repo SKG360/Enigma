845b285e6baab6f700e75ecf07ffac7748293a17
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-08-28 14:40:18 -0600

    update encrypt, decrypt

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -3,23 +3,28 @@
 require './lib/enigma'
 require 'pry'
 
-encrypted_filename        = ARGV[0]
-decrypted_file_desination = ARGV[1]
-decryption_key            = ARGV[2]
-decryption_date           = ARGV[3]
-date = Date.today.strftime("%d%m%y")
+# encrypted_filename        = ARGV[0]
+# decrypted_file_desination = ARGV[1]
+# decryption_key            = ARGV[2]
+# decryption_date           = ARGV[3]
+# date = Date.today.strftime("%d%m%y")
+
+encrypted_filename         = "encrypted.txt"
+decrypted_file_destination = "decrypted.txt"
+decryption_key             = "12345"
+decryption_date            = "280818"
 
 
 def read_from_file(file)
     File.read(file)
 end # => "6895k9gk5cy5r3fr6yzlrb4ll"
 
-def write_to_file(text)
-    File.open(@encrypted_file_destination,'w') { |file| file.write(text)}
+def write_to_file(destination, text)
+    File.open(destination,'w') { |file| file.write(text)}
 end
 
-
 enigma = Enigma.new
-
-decrypted_string = enigma.decrypt(read_from_file(encrypted_file), encryption_key, encryption_date)
-write_to_file(decrypted_string)
+encrypted_message = read_from_file(encrypted_filename)
+# decrypted_string = enigma.decrypt(encrypted_message, decryption_key, decryption_date)
+decrypted_string = "Hello Nurse!"
+write_to_file(decrypted_file_destination, decrypted_string)
--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -5,66 +5,40 @@ require 'date'
 
 date  = Date.today.strftime("%d%m%y")
 
-class Encrypt
-  def initialize(input_filename, output_filename)
-    @input_filename = input_filename
-    @output_filename = output_filename
-  end
-
-  def check_local_files
-    Dir.glob("*.txt")
-  end
-
-  def found_file?
-    check_local_files.include?(@input_filename)
-  end
-
-  def found_duplicate_file?
-    check_local_files.include?(@output_filename)
-  end
-
-  def read_from_file(file)
-    if found_file?
-      @message_text = File.read(file)
-    else
-      "ERROR: File not found"
-    end
-  end
-
-  def display_warning_and_option(encrypted_string)
-    p "WARNING: the target file already exists. Do you want to overwrite? (y)es, (n)o"
-    print ">"
-    #can't use gets because of terminal args
-    reply = $stdin.gets.chomp.to_s
-    if reply == 'y'
-      File.open(@output_filename,'w') { |file| file.write(encrypted_string)}
-    end
-  end
-
-  def write_to_file(encrypted_string)
-    if found_duplicate_file?
-      display_warning_and_option(encrypted_string)
-    elsif
-      File.open(@output_filename,'w') { |file| file.write(encrypted_string)}
-    end
-  end
-end
-
 key = "82648"
 date = "250818"
 
+# default
+# # added for convenience
+# if ARGV[0] == nil
+#   ARGV[0] = "message.txt"
+# end
+
+# if ARGV[1] == nil
+#   ARGV[1] = "encrypted.txt"
+# end
+
+
+unencrypted_file_name            = ARGV[0]
+encrypted_file_destination       = ARGV[1]
+
+
+def read_from_file(file)
+  File.read(file)
+end # => "6895k9gk5cy5r3fr6yzlrb4ll"
+
+def write_to_file(destination, text)
+  File.open(destination,'w') { |file| file.write(text)}
+end
+
+
 enigma = Enigma.new
-encrypted_message = enigma.encrypt("this is so secret ..end..", key, date)
+raw_message = read_from_file(unencrypted_file_name)
+# encrypted_message = enigma.encrypt(raw_message, key, date)
+encrypted_message = "this is so secret ..end.."
+encrypted_file_destination = "encrypted.txt"
 
-# added for convenience
-if ARGV[0] == nil
-  ARGV[0] = "message.txt"
-end
 
-if ARGV[1] == nil
-  ARGV[1] = "encrypted.txt"
-end
+my_message = encrypted_message
+write_to_file(encrypted_file_destination, my_message)
 
-e = Encrypt.new(ARGV[0], ARGV[1])
-my_message = e.read_from_file(ARGV[0])
-e.write_to_file(encrypted_message)
