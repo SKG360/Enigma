34eee9742863c53d6558695e73e9687ca64fb984
SKG360 <samuel.giles@colorado.edu>
2018-08-28 14:54:57 -0600

    Updates file i_o

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -2,20 +2,27 @@ require './lib/enigma'
 require 'pry'
 
 encrypted_filename        = ARGV[0]
-decrypted_file_desination = ARGV[1]
+decrypted_file_destination = ARGV[1]
 decryption_key            = ARGV[2]
 decryption_date           = ARGV[3]
+date = Date.today.strftime("%d%m%y")
+
+# encrypted_filename         = "encrypted.txt"
+# decrypted_file_destination = "decrypted.txt"
+# decryption_key             = "12345"
+# decryption_date            = "280818"
 
 
-class Decrypt 
-    def initialize(encrypted_filename, 
-            decrypted_file_desination,
-            decryption_key,
-            decryption_date)
+def read_from_file(file)
+    File.read(file)
+end # => "6895k9gk5cy5r3fr6yzlrb4ll"
 
-        encrypted_filename        = @encrypted_filename 
-        decrypted_file_desination = @decrypted_file_desination
-        decryption_key            = @decryption_key
-        decryption_date           = @decryption_date
-    end
+def write_to_file(destination, text)
+    File.open(destination,'w') { |file| file.write(text)}
 end
+
+enigma = Enigma.new
+encrypted_message = read_from_file(encrypted_filename)
+# decrypted_string = enigma.decrypt(encrypted_message, decryption_key, decryption_date)
+decrypted_string = "Hello Nurse!"
+write_to_file(decrypted_file_destination, decrypted_string)
